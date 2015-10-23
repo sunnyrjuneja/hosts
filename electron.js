1499f180fa9bdb4763bc27b2f61afb20b59017a4
Sunny Juneja <me@sunnyjuneja.com>
2015-10-23 12:58:03 -0700

    Add standard. Fix warnings.

--- electron.js
+++ electron.js
@@ -1,23 +1,23 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+var app = require('app')
+var BrowserWindow = require('browser-window')
 
-require('crash-reporter').start();
+require('crash-reporter').start()
 
-var mainWindow = null;
+var mainWindow = null
 
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  mainWindow.openDevTools();
+  mainWindow.loadUrl('file://' + __dirname + '/index.html')
+  mainWindow.openDevTools()
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
-});
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+})
